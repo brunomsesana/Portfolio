42611ea08f2105af8f25bcfe54efedfd836c1ed5
BrunimMS <38635661+brunomsesana@users.noreply.github.com>
2024-07-02 19:29:15 -0300

    0.13-Alpha
    Atualizações gerais

--- CoCSheet/script.js
+++ CoCSheet/script.js
@@ -576,17 +576,36 @@ function createopts(){
         document.getElementById('sheetnames').appendChild(opt)
     }
 }
-let modClaro = "Modo Claro";
-let modEscuro = "Modo Escuro";
+let modAtual = 0;
 function claro(el){
     document.getElementsByTagName("body")[0].classList.add("claro");
     el.setAttribute("onclick", "escuro(this)");
-    el.innerHTML = modEscuro;
+    document.getElementById("bgMod").style.fill = "#38b6ff";
+    let els1 = document.getElementsByClassName("light");
+    let els = document.getElementsByClassName("dark");
+    for(let i = 0; i < els.length; i++){
+        els[i].style.opacity = 0;
+        els[i].style.translate = "40px";
+    }
+    for(let i = 0; i < els1.length; i++){
+        els1[i].style.opacity = 1
+        els1[i].style.translate = "0px";
+    }
     modAtual = 1;
 }
 function escuro(el){
     document.getElementsByTagName("body")[0].classList.remove("claro");
     el.setAttribute("onclick", "claro(this)");
-    el.innerHTML = modClaro;
+    document.getElementById("bgMod").style.fill = "#737373";
+    let els = document.getElementsByClassName("light");
+    let els1 = document.getElementsByClassName("dark");
+    for(let i = 0; i < els.length; i++){
+        els[i].style.opacity = 0;
+        els[i].style.translate = "-40px";
+    }
+    for(let i = 0; i < els1.length; i++){
+        els1[i].style.opacity = 1;
+        els1[i].style.translate = "0px";
+    }
     modAtual = 0;
 }
