1dc513a900a5b9486d96ab41cde2561455b6bad6
BrunimMS <38635661+brunomsesana@users.noreply.github.com>
2024-07-01 22:58:41 -0300

    0.11-Alpha
    Ajustes nas cores do CoCSheet especificamente e adição do link na pagina do portfolio

--- CoCSheet/script.js
+++ CoCSheet/script.js
@@ -576,3 +576,17 @@ function createopts(){
         document.getElementById('sheetnames').appendChild(opt)
     }
 }
+let modClaro = "Modo Claro";
+let modEscuro = "Modo Escuro";
+function claro(el){
+    document.getElementsByTagName("body")[0].classList.add("claro");
+    el.setAttribute("onclick", "escuro(this)");
+    el.innerHTML = modEscuro;
+    modAtual = 1;
+}
+function escuro(el){
+    document.getElementsByTagName("body")[0].classList.remove("claro");
+    el.setAttribute("onclick", "claro(this)");
+    el.innerHTML = modClaro;
+    modAtual = 0;
+}
