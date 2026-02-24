774735501aa5153f349f7232e3e5e480bd15ef10
Bruno Sesana <38635661+brunomsesana@users.noreply.github.com>
2026-02-24 14:43:17 -0300

    1.31
    Ajuste da idade

--- src/Components/SobreMim/index.tsx
+++ src/Components/SobreMim/index.tsx
@@ -13,7 +13,7 @@ export default function SobreMim(){
                 <Image source={Photo}></Image>
                 <div className={styles.textos}>
                     <h2 className='typer'>{lang == 'pt-BR' ? "Sobre mim" : "About Me"}</h2>
-                    <p>{lang == "pt-BR" ? "Meu nome é Bruno Machado Sesana, tenho 20 anos e sempre fui apaixonado por tecnologia, querendo criar coisas novas com ela. Comecei programando jogos, com meus 12 anos e fui evoluindo nas diferentes linguagens. Hoje tento focar mais na programação web, mas sigo evoluindo em outras areas da programação." : "My name is Bruno Machado Sesana, I'm 20 years old and I have always been in love for technology, wanting to create new things with it. I started with game development when I was 12, and I've been improving in the different programming languages throught the years. At the moment I'm focusing in web development, but I'm also enhancing my skills in other programming areas."}</p>
+                    <p>{lang == "pt-BR" ? "Meu nome é Bruno Machado Sesana, tenho 21 anos e sempre fui apaixonado por tecnologia, querendo criar coisas novas com ela. Comecei programando jogos, com meus 12 anos e fui evoluindo nas diferentes linguagens. Hoje tento focar mais na programação web, mas sigo evoluindo em outras areas da programação." : "My name is Bruno Machado Sesana, I'm 21 years old and I have always been in love for technology, wanting to create new things with it. I started with game development when I was 12, and I've been improving in the different programming languages throught the years. At the moment I'm focusing in web development, but I'm also enhancing my skills in other programming areas."}</p>
                 </div>
             </div>
             <Scroll></Scroll>
